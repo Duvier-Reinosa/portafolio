d42cbb68a3271466adee76af2d061b1413f75c47
Duvier-Reinosa <duvier.reinosa@integ.ro>
2024-07-09 23:47:53 -0500

    Feature(cvButton)[Added new button for download cv]

--- src/components/cvButton/CvButton.tsx
+++ src/components/cvButton/CvButton.tsx
@@ -2,9 +2,6 @@ import './cvButton.css'
 
 function CvButton() {
     const handleDownload = () => {
-        // window.open('https://drive.google.com/file/d/1XzPZj1jU0p8z0nR4bZ2J1Bbq2l8Z7K8J/view?usp=sharing')
-        // necesito descargar el cv.pdf de la carpeta public
-
         window.open('/cv.pdf')
     }
 
